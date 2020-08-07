04af47075166967f9fddc35d53d132f86899c95f
Felipe Moura <felipemimoura@gmail.com>
2020-08-07 15:48:41 -0300

    projeto Completo

---
+++ src/database/fake_data.js
@@ -0,0 +1,41 @@
+const proffys = [
+  {
+    name: 'Diego Fernandes',
+    avatar:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4',
+    whatsapp: '999999999',
+    bio:
+      'Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.',
+    subject: 'Química',
+    cost: '20',
+    weekday: [0],
+    time_from: [720],
+    time_to: [1220],
+  },
+  {
+    name: 'Joseph Oliveira',
+    avatar:
+      'https://avatars0.githubusercontent.com/u/39345247?s=460&u=cdff2624a327a43e2765112a54e966a06eac6d79&v=4',
+    whatsapp: '999999999',
+    bio:
+      'Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.',
+    subject: 'Química',
+    cost: '20',
+    weekday: [1],
+    time_from: [720],
+    time_to: [1220],
+  },
+  {
+    name: 'Fernando Graciano',
+    avatar:
+      'https://avatars3.githubusercontent.com/u/44657781?s=460&u=1ff2267a9ec077ba348d6a52d7e8725a78360a2d&v=4',
+    whatsapp: '999999999',
+    bio:
+      'Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.',
+    subject: 'Química',
+    cost: '20',
+    weekday: [1],
+    time_from: [720],
+    time_to: [1220],
+  },
+];
